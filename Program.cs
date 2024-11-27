75a8a41f8989fa026e9e3f88842df6dbcf123cf6
Stella <stella.jin123@gmail.com>
2024-11-27 21:50:06 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+public class ProductList
+{
+    // properties
+    public string Category { get; set; }
+    public string ProductName { get; set; }
+    public int Price { get; set; }
+    // constructor
+    public ProductList(string category, string name, int price)
+    {
+        Category = category;
+        ProductName = name;
+        Price = price;
+    }
+    // method
+    public void Print()
+    {
+        Console.WriteLine($"{Category},{ProductName},{Price}");
+    }
+    public static void Main()
+    {
+        List<ProductList> productList = new List<ProductList>();
+
+        while (true)
+        {
+            Console.WriteLine("To enter a new product- follow the steps | To quite - enter:'Q'");
+
+            Console.Write("Enter a category: ");
+            string categoryInput = Console.ReadLine();
+            if (categoryInput.ToUpper().Trim() == "Q")
+            {
+                break;
+            }
+            Console.Write("Enter a Product Name: ");
+            string productNameInput = Console.ReadLine();
+            if (productNameInput.ToUpper().Trim() == "Q")
+            {
+                break;
+            }
+            Console.Write("Enter a Price: ");
+            int.TryParse(Console.ReadLine(), out int priceInput);
+            if (productNameInput.ToUpper().Trim() == "Q")
+            {
+                break;
+            }
+
+
+        }
+
+    }
+}
