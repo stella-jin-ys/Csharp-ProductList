bd6d8f40ad9465ec7d05f8c8f0545367aa344c2d
Stella <stella.jin123@gmail.com>
2024-11-28 15:56:11 +0100

    add products to the productList and sort by price

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Linq;
 
 public class ProductList
 {
@@ -6,6 +6,7 @@ public class ProductList
     public string Category { get; set; }
     public string ProductName { get; set; }
     public int Price { get; set; }
+
     // constructor
     public ProductList(string category, string name, int price)
     {
@@ -14,41 +15,64 @@ public class ProductList
         Price = price;
     }
     // method
+
     public void Print()
     {
-        Console.WriteLine($"{Category},{ProductName},{Price}");
+        Console.WriteLine($"{Category.PadRight(30)}{ProductName.PadRight(30)}{Price.ToString().PadRight(30)}");
+
     }
-    public static void Main()
+
+}
+
+class Program
+{
+    static void Main()
     {
         List<ProductList> productList = new List<ProductList>();
 
         while (true)
         {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("To enter a new product- follow the steps | To quite - enter:'Q'");
+            Console.ResetColor();
 
             Console.Write("Enter a category: ");
             string categoryInput = Console.ReadLine();
             if (categoryInput.ToUpper().Trim() == "Q")
             {
+                Console.WriteLine("----------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Category".PadRight(30) + "Product".PadRight(30) + "Price".PadRight(30));
+                Console.ResetColor();
+
+                // Sort products by price in ascending order
+                productList.Sort((x, y) => x.Price.CompareTo(y.Price)); // Lambda for comparison
+                foreach (var product in productList)
+                {
+                    product.Print();
+
+                }
+                int totalPrice = productList.Sum(product => product.Price)
+; Console.WriteLine("\n" + "".PadRight(30) + "Total amount: ".PadRight(30) + totalPrice);
+                Console.WriteLine("----------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("To enter a new product - enter: 'P' | To search for a product - enter: 'S' | To quit - enter: 'Q'");
+                Console.ResetColor();
                 break;
             }
+
             Console.Write("Enter a Product Name: ");
             string productNameInput = Console.ReadLine();
-            if (productNameInput.ToUpper().Trim() == "Q")
-            {
-                break;
-            }
+
             Console.Write("Enter a Price: ");
             int.TryParse(Console.ReadLine(), out int priceInput);
-            if (productNameInput.ToUpper().Trim() == "Q")
-            {
-                break;
-            }
+
             productList.Add(new ProductList(categoryInput, productNameInput, priceInput));
-            Console.WriteLine("The product was successfully added");
-            break;
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The product was successfully added!");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.ResetColor();
         }
-
     }
 }
+
